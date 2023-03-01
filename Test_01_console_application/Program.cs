98ab82a801a498d9a939b4bbece359b9c46268a0
Bongjun Kim <kbjun22@gmail.com>
2023-03-02 00:48:30 +0900

    초기 커밋.

---
+++ Test_01_console_application/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_01_console_application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World");
+            Console.ReadKey();
+                       
+        }
+    }
+}
